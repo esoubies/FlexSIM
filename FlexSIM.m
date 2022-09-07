52ad36ca15d0aa908fab433f88d164eb0851570b
Alejandro Noguerón <alex.noguerona@gmail.com>
2022-09-07 13:13:13 +0200

    First commit with dir structure and empty params.m and FlexSIM.m

---
+++ FlexSIM.m
@@ -0,0 +1,4 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FlexSIM - to be called from the file params.m and not modified
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+addpath(genpath(pwd))                   % Add subfolders to path
