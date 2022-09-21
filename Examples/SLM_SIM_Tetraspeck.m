926c9643eb40d91f889bb25d0ddf33012cad8ecf
E. Soubies <emmanuel.soubies@irit.fr>
2022-09-21 18:24:54 +0200

    add Tetraspeck example

---
+++ Examples/SLM_SIM_Tetraspeck.m
@@ -0,0 +1,64 @@
+%--------------------------------------------------------------------------
+% FlexSIM reconstruction script of the SLM-SIM Tetraspeck data provided at
+% https://github.com/fairSIM/test-datasets/blob/master/SLM-SIM-Bielefeld.md
+%
+% Copyright (2022) A. Nogueron (anogueron.1996@gmail.com)
+%                  E. Soubies (emmanuel.soubies@irit.fr) 
+%--------------------------------------------------------------------------
+clear; close all; clc;
+
+%% General parameters
+% -- Path and files
+params.DataPath = fullfile(pwd,'SLM_SIM_Tetraspeck.tif');   % Path to the SIM stack 
+params.pathToFlexSIM = '../';                             % Path to the root of GitHub FlexSIM repo
+
+% -- Display and saving
+params.displ = 1;                       % Displaying choice, from 0 to 2 with increasing number of display
+params.sav = 1;                         % Boolean if true save the result
+
+%% Data related parameters
+% -- Properties of the SIM data stack
+params.AcqConv= 'paz';                  % Phase (p), angle (a) and time (z) convention. Choose one of ('paz', 'pza' or 'zap')
+params.nbOr = 4;                        % Number of orientations
+params.nbPh = 3;                        % Number of phases 
+
+% -- OTF Approximation
+params.lamb = 680;     % Emission wavelength
+params.res = 97.6;     % Pixel size (nm)
+params.Na = 1.2;       % Objective numerica aperture
+params.damp = 0.4;     % damping parameter (in [0,1], 1= no damping) to attenuate middle freq in the approx OTF
+
+%% FlexSIM parameters
+% -- Patch-based processing
+params.szPatch=0;                 % If >0, FlexSIM will perform pattern estimation and reconstruction by patches of size 'szPatch'
+params.overlapPatch=0;            % Overlap between patches if szPatch>0
+params.enhanceContrast=0;         % When 0 (and patch-based) rescale the reconstruction to widefield intensity variations. When 1, keep enhanced contrast due to patch-based rec
+
+% -- Parameters for patterns estimation
+params.roi = [128 128 256];       % Select ROI for pattern estimation ([initial y-coord, initial x-coord, size])
+params.limits = [0.45, 0.55];     % Ring over which the J function is evaluated for initializing (fc = 1)
+params.ringMaskLim = [0, 1.1];    % Lower and upper limit of mask to finish hiding WF component, givien as factor of fc
+params.nMinima = 2;               % Number of starting points for the refinement steps
+params.nPoints = 150;             % Number of points in the J evaluation grid
+params.FilterRefinement = 1;      % Number of times that the filter is upgraded (gradient descent cycles)
+params.method = 2;                % Method : 0 - treat all images independently
+                                  %          1 - use all images with same orientation to estimate a unique wavevector
+                                  %          2 - 1 + assume equally spaced phases
+params.estiPattLowFreq=0;         % If true, estimate the low-freq component of the patterns
+                                  
+% -- Parameters for image Reconstruction 
+params.sepOrr = 0;                % Boolean if true treat each orientation separately
+params.padSz=20;                  % Padding size for the optimization variable (to account for boundaries effects)
+params.mu =  1e-5;                % Regularization parameter
+params.regType=1;                 % Choice regul: 1 for Tikhonov (i.e., Wiener), 2 for Total Variation, 3 for Good roughness
+params.maxIt = 150;               % Maximum number of iterations (stopping criteria)
+params.stepTol = 5e-4;            % Relative error tolerance between two iterates (stopping criteria)
+
+%% Run FlexSIM
+run(strcat(params.pathToFlexSIM, '/InstallFlexSIM.m')) % Take care of paths & GlobalBioIm
+res = FlexSIM(params);                                 % Run FlexSIM
+
+
+
+
+
