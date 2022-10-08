b4b5d93813438fd59d7145263402c65b76dec7b7
E. Soubies <emmanuel.soubies@irit.fr>
2022-10-08 14:01:19 +0200

    remove FT in DisplayReconstruction

--- src/General/DisplayReconstruction.m
+++ src/General/DisplayReconstruction.m
@@ -2,10 +2,8 @@ function fig_id=DisplayReconstruction(rec,wf,fig_id)
 
 if ~isgraphics(fig_id), fig_id=figure; end
 set(0,'CurrentFigure',fig_id);
-s1=subplot(2,2,1);imdisp(rec,'SIM Reconstruction',0);
-s2=subplot(2,2,2);imdisp(wf,'Widefield',0);
-s3=subplot(2,2,3);imdisp(log10(abs(fftshift(fft2(rec)+1))),'SIM Reconstruction (FT)',0);
-s4=subplot(2,2,4);imdisp(log10(abs(fftshift(fft2(wf)+1))),'Widefield (FT)',0);
+s1=subplot(1,2,1);imdisp(rec,'SIM Reconstruction',0);
+s2=subplot(1,2,2);imdisp(wf,'Widefield',0);
 Link = linkprop([s1, s2],{'CameraUpVector', 'CameraPosition', 'CameraTarget', 'XLim', 'YLim', 'ZLim'});
 setappdata(gcf, 'StoreTheLink', Link);drawnow;
 end
