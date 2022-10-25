4598a09c958ab2f8e4f3a45efda9da8747a2038a
E. Soubies <emmanuel.soubies@irit.fr>
2022-10-25 19:04:54 +0200

    add an alternative to SliceViewer in case matlab version if not recent enough

--- src/General/DisplayStack.m
+++ src/General/DisplayStack.m
@@ -1,7 +1,11 @@
 function fig_id=DisplayStack(stack,titl,fig_id)
 if ~isgraphics(fig_id), fig_id=figure; end
 set(0,'CurrentFigure',fig_id);
-sliceViewer(stack);
+if exist('sliceViewer')
+    sliceViewer(stack);
+else
+    imagesc(stack(:,:,1));axis image; axis off; colormap gray;
+end
 title(titl);
 drawnow;set(gcf,'Visible','on');
 end
