9a5d6a41b97169afdc72ef85195e96f2e223980a
Alejandro Noguerón <alex.noguerona@gmail.com>
2022-10-26 17:17:53 +0200

    Added `gather` to SliceViewer call to avoid problem when using GPU

--- src/General/DisplayStack.m
+++ src/General/DisplayStack.m
@@ -2,7 +2,7 @@ function fig_id=DisplayStack(stack,titl,fig_id)
 if ~isgraphics(fig_id), fig_id=figure; end
 set(0,'CurrentFigure',fig_id);
 if exist('sliceViewer')
-    sliceViewer(stack);
+    sliceViewer(gather(stack));
 else
     imagesc(stack(:,:,1));axis image; axis off; colormap gray;
 end
