57c771a48b1e2c8d4200e41549fa98d1eeabdd2e
E. Soubies <emmanuel.soubies@irit.fr>
2023-01-19 14:11:05 +0100

    update the grid-based init to match the theoretical derivation presented in the paper

--- src/EstimatePatterns/CrossCorr.m
+++ src/EstimatePatterns/CrossCorr.m
@@ -1,6 +1,6 @@
-function [corr,K1,K2] = CrossCorr(G,wf,params)
+function [Jmap,K1,K2] = CrossCorr(G,wf,params)
 %--------------------------------------------------------------------------
-% Function [corr,K1,K2] = CrossCorr(G,wf,params)
+% Function [Jmap,K1,K2] = CrossCorr(G,wf,params)
 %
 % Compute initial wave-vector through cross-correlation between the
 % widefield image and the raw SIM images in Fourier domain.
@@ -12,14 +12,18 @@ function [corr,K1,K2] = CrossCorr(G,wf,params)
 %                         - Na: Objective numerica aperture
 %                         - res: resolution of the SIM data stac
 %         
-% Outputs : corr   -> Correlation map
+% Outputs : Jmap   -> Grid-based evaluation of J in [1]
 %           K1, K2 -> grid on which the correl map is evaluated
 %
-% Copyright (2022) A. Nogueron (anogueron.1996@gmail.com) , E. Soubies 
-% (emmanuel.soubies@irit.fr) 
+% [1] FlexSIM: ADD REF TO PAPER
+%
+% Copyright (2022) A. Nogueron (anogueron.1996@gmail.com), 
+%                  E. Soubies (emmanuel.soubies@irit.fr) 
 %--------------------------------------------------------------------------
 
 % -- Pre-computations
+fftshift_ = @(x) fftshift(fftshift(x,1),2);
+ifftshift_ = @(x) ifftshift(ifftshift(x,1),2);
 FCut = 2*params.Na/params.lamb*params.res;            % Cut-off frequency
 sz=size(wf);fac=4;
 fftwf=fft2(padarray(wf,sz(1:2)*fac,'post'));
@@ -27,14 +31,26 @@ fftG=fft2(padarray(G,sz(1:2)*fac,'post'));
 sz=size(fftwf);
 
 % -- Perform cross-correlation btw the fft of wf and the fft of processed data G
+% tmp=fft2(ifftshift(fftwf)).*conj(fft2(ifftshift_(fftG)));
+% corr=fftshift_(ifft2(tmp));
+% corrSymConj=fftshift_(ifft2(conj(tmp)));
+% if params.eqPh
+%     wght=reshape(exp(-2*1i*[0:params.nbPh-1]*pi/params.nbPh),[1,1,params.nbPh]);
+%     tmp=mean((corr + corrSymConj).*wght,3);
+%     Jmap=MaskFT(abs(tmp).^2,FCut,params.ringMaskLim);
+% else
+%     tmp=corr + corrSymConj;
+%     Jmap=MaskFT(mean(abs(tmp).^2,3),FCut,params.ringMaskLim);
+% end
+
 corrtmp=fftshift(ifft2((fft2(ifftshift(fftwf))).*conj(fft2(ifftshift(fftG)))));
 if params.eqPh
     wght=reshape(exp(-2*1i*[0:params.nbPh-1]*pi/params.nbPh),[1,1,params.nbPh]);
     tt=mean(corrtmp.*wght,3);tt2=conj(tt)./abs(tt);
-    corr=MaskFT(real(mean(corrtmp.*wght,3).*tt2),FCut,params.ringMaskLim);
+    Jmap=MaskFT(real(mean(corrtmp.*wght,3).*tt2),FCut,params.ringMaskLim);
 else
     tt=conj(corrtmp)./abs(corrtmp); 
-    corr=MaskFT(real(mean(corrtmp.*tt,3)),FCut,params.ringMaskLim);
+    Jmap=MaskFT(real(mean(corrtmp.*tt,3)),FCut,params.ringMaskLim);
 end
 
 % Generate grid and get radius
