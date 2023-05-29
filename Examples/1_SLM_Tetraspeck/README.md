de377905c39552dd8190bfab8a3336566ad2aa0c
E. Soubies <emmanuel.soubies@irit.fr>
2023-05-29 11:07:16 +0200

    Finalize example SLM-SIM Tetraspeck

--- Examples/1_SLM_Tetraspeck/README.md
+++ Examples/1_SLM_Tetraspeck/README.md
@@ -1,30 +1,10 @@
-# Tetraspeck - fair-SIM
+# SLM-SIM Tetraspeck (FairSIM)
+
+Presented in the fairSIM original publication, this dataset shows tetraspeck structures imaged with SLM-SIM. 
+For a more homogeneus resolution improvement, 12 images (4 orientations with 3 phases each) were acquired. 
+The dataset can be dowloaded from [here](https://github.com/fairSIM/test-datasets/releases/download/SLM-SIM-Bielefeld/SLM-SIM_Tetraspeck200_680nm.tif).
 
 [1] M. Müller, V. Mönkemöller, S. Hennig, W. Hübner, and T. Huser, “Open-source image reconstruction of super-resolution structured illumination microscopy data in ImageJ,” Nat Commun, vol. 7, no. 1, Art. no. 1, Mar. 2016, doi: 10.1038/ncomms10980
 
-### Overview
 
-Presented in the fairSIM original publication, this dataset shows tetraspeck structures imaged with SLM-SIM. For a more homogeneus resolution improvement, 12 images (4 orientations with 3 phases each) were acquired. The dataset can be dowloaded from [here](https://github.com/fairSIM/test-datasets/releases/download/SLM-SIM-Bielefeld/SLM-SIM_Tetraspeck200_680nm.tif).
 
-### Imaging Parameters
-
-<img src="https://latex.codecogs.com/gif.latex?\lambda: " /> 525
-
-Pixel size: 97.6 nm
-
-_NA_: 1.49
-
-### Patter Parameters Estimation
-
-We reconstructed this data set with _flexSIM_ and _fairSIM_, estimating with the pattern estimation parameters described below.
-
-| Method  | K_x  [px] | K_y [px] | Phase [rad] | OTF Att Strength (FWHM) | OTF Comp | 
-| ------------- |:-------------:| ------------- | ------------- | | |
-| flexSIM (or 1) | 2.123 | -91.227 | 1.632 | 0.99 (0.1) | 0.3 |
-| flexSIM (or 2) | 92.066 | 1.836 | 0.556 | | |
-| flexSIM (or 3) | 61.982 | 64.372 | 0.720 | | |
-| flexSIM (or 4) | 65.046| -62.165 | 0.456 | | |
-| fairSIM (or 1) | 2.1 | -91.211 | 0.310 | - | 0.15 |
-| fairSIM (or 2) | 92.078 | 1.833 | 1.423 |  | |
-| fairSIM (or 3) | 62.033 | 64.389 | 0.060 | |
-| fairSIM (or 4) | 65.122 | -62.100 | 0.954 | |
--- Examples/1_SLM_Tetraspeck/SLM_Tetraspeck.m
+++ Examples/1_SLM_Tetraspeck/SLM_Tetraspeck.m
@@ -18,10 +18,11 @@ params.pathToFlexSIM = '../../';                                % Path to the ro
 if ~exist(params.DataPath, 'file')
     websave(params.DataPath, 'https://github.com/fairSIM/test-datasets/releases/download/SLM-SIM-Bielefeld/SLM-SIM_Tetraspeck200_680nm.tif');
 end
+
 % -- Display, saving and GPU acceleration
 params.displ = 1;                       % Displaying choice, from 0 to 2 with increasing number of display
 params.verbose=2;                       % 0: minimal text displays / 1: detailled text displays / 2: more details
-params.sav = 1;                         % Boolean if true save the result
+params.sav = 1;                         % Boolean on whether to save or not the reconstructed image and estimated patterns
 params.GPU = 0;                         % Boolean on whether to use GPU or not
 
 %% Data related parameters
@@ -29,45 +30,39 @@ params.GPU = 0;                         % Boolean on whether to use GPU or not
 params.SzRoiBack=13;        % Size (odd number or empty) of the ROI for background estimation (position automatically detected so as to minimize the intensity within the ROI)
 
 % -- Patterns
-params.StackOrder= 'pa';    % Phase (p), angle (a) and time (z) convention. Choose one of ('paz', 'pza' or 'zap')
+params.StackOrder = 'pa';   % SIM stack order ('ap','pa','apw','paw','wap','wpa') with a=angles, p=phases, w=widefield. For 'ap' and 'pa' a widefield per orientation is computed as the summing along phases. 
 params.nbOr = 4;            % Number of orientations
 params.nbPh = 3;            % Number of phases 
-params.pattAmp=1;           % Amplitude of the patterns in [0,1]
-
 
 % -- OTF Approximation
-params.lamb = 680;     % Emission wavelength
+params.lamb = 680;     % Emission wavelength (nm)
 params.res = 97.6;     % Pixel size (nm)
 params.Na = 1.2;       % Objective numerica aperture
 params.damp = 0.4;     % damping parameter (in [0,1], 1= no damping) to attenuate middle freq in the approx OTF
 
 %% Parameters for patterns estimation
 params.SzRoiPatt = 257;           % Size (odd number or empty) of the ROI for pattern estimation (position automatically detected so as to maximize the intensity within the ROI)
-params.limits = [0.45, 0.55];     % Ring over which the J function is evaluated for initializing (fc = 1)
-params.ringMaskLim = [0, 1];      % Lower and upper limit of mask to finish hiding WF component, givien as factor of fc
-params.nMinima = 1;               % Number of starting points for the refinement steps
-params.nPoints = 0;               % Number of points in the J evaluation grid. If set to 0, initialization is done via cross-correlation in Fourier domain.
-params.FilterRefinement = 1;      % Number of times that the filter is upgraded (gradient descent cycles)
-params.method = 2;                % Method : 0 - treat all images independently
-                                  %          1 - use all images with same orientation to estimate a unique wavevector
-                                  %          2 - 1 + assume equally spaced phases
+params.ringMaskLim = [0, 1];      % Lower and upper limits of Fourier ring mask (given as factor of the cutoff freq)
+params.eqPh = 1;                  % Boolean, if true equally-spaced phases are assumed
+params.nMinima = 1;               % Number of initial wavevectors extracted from cross-correlation maps
 params.estiPattLowFreq=0;         % If true, estimate the low-freq component of the patterns
-                                  
+params.pattAmp=1;                 % Amplitude of the patterns in [0,1]
+                       
 %% Parameters for image Reconstruction 
 % -- Patch-based processing
-params.szPatch=0;                 % If >0, FlexSIM will perform the reconstruction by patches of size 'szPatch'
-params.overlapPatch=0;            % Overlap between patches if szPatch>0
-params.parallelProcess=0;         % If 1, paralellizes the loop over patches
+params.szPatch=0;                 % Size (px) of patches (if 0, no patch-based processing)
+params.overlapPatch=0;            % Overlap between patches
+params.parallelProcess=0;         % Boolean on whether to parallelize the loop over patches
 
 % -- OTF Attenuation
-params.OTFAttStr=0;               % Strenght of the OTF attenuation (in [0,1]). If 0 no OTF attenuation.
+params.OTFAttStr=0;               % Strength of the OTF attenuation (in [0,1]). If 0 no OTF attenuation.
 params.OTFAttwdth=0;              % Width of the OTF attenuation (>0). If 0 no OTF attenuation.
 
 % -- Operators and Costs
-params.sepOrr = 0;                % Boolean if true treat each orientation separately
-params.padSz=20;                  % Padding size for the optimization variable (to account for boundaries effects)
+params.sepOrr = 0;                % Boolean on whether to treat each orientation separately
+params.padSz=20;                  % Padding size (px) used in the forward operator
 params.mu =  5e-5;                % Regularization parameter
-params.regType=1;                 % Choice regul: 1 for Tikhonov (i.e., Wiener), 2 for Total Variation, 3 for Good roughness
+params.regType=1;                 % Regularization function: 1 for 1st-order Tikhonov, 2 for Total Variation, 3 for Good roughness.
 
 % -- Optim
 params.maxIt = 100;               % Maximum number of iterations (stopping criteria)
