9cb41a87aa27cd247726d5179cc5699f21ed1ed6
E. Soubies <emmanuel.soubies@irit.fr>
2024-02-26 11:19:48 +0100

    fix pb in display Patt

--- src/General/DisplayPattParams.m
+++ src/General/DisplayPattParams.m
@@ -42,7 +42,11 @@ leg{end+1}='Ring Region Search';
 leg{end+1}='Mask WF';
 col=[0 0.4470 0.7410
 0.8500 0.3250 0.0980
-0.9290 0.6940 0.1250];
+0.9290 0.6940 0.1250
+0.4940    0.1840    0.5560
+0.4660    0.6740    0.1880
+0.3010    0.7450    0.9330
+0.6350    0.0780    0.1840];
 for i = 1:size(k, 1)
     tmp = k(i, :) .* sz_y(2:-1:1) * params.res / pi + sz_y(2:-1:1)/2+1;
     h(end+1)=plot(ax,tmp(1), tmp(2), 'o', 'MarkerSize', 8, 'LineWidth', 3,'Color',col(i,:));
